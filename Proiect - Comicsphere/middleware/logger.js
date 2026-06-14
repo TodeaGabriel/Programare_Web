fbd29525a313a79e8328107a112a3d54626664c7
TodeaGabriel <todeagabi13@gmail.com>
2026-06-14 16:12:25 +0300

    modificare proiect

--- Proiect - Comicsphere/middleware/logger.js
+++
@@ -1,8 +0,0 @@
-// middleware/logger.js
-const logger = (req, res, next) => {
-    const userEmail = req.session && req.session.user ? req.session.user.email : "anonim";
-    console.log(`${req.method} ${req.url} - user: <${userEmail}>`);
-    next(); // Trecem la următorul middleware/rută
-};
-
-module.exports = logger;
